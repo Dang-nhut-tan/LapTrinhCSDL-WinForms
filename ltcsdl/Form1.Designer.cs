8a0f21cba2c794f348d1c05ebd197de6a7909403
Dang Nhut Tan <tan128340@gmail.com>
2026-01-27 22:52:08 +0700

    nghịch git hub

--- ltcsdl/Form1.Designer.cs
+++ ltcsdl/Form1.Designer.cs
@@ -28,12 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lb_1 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // lb_1
+            // 
+            lb_1.AutoSize = true;
+            lb_1.BackColor = SystemColors.ControlLight;
+            lb_1.Location = new Point(382, 61);
+            lb_1.Name = "lb_1";
+            lb_1.Size = new Size(0, 20);
+            lb_1.TabIndex = 0;
+            lb_1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(361, 253);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(lb_1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lb_1;
+        private Button button1;
     }
 }
--- ltcsdl/Form1.cs
+++ ltcsdl/Form1.cs
@@ -6,5 +6,15 @@ namespace ltcsdl
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lb_1.Text = "hello world";
+        }
     }
 }
